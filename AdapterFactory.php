560783742b3bad68ecb91946c57f2ede9a18eccd
boite <boite@users.noreply.github.com>
2019-05-30 15:28:55 +0100

    Avoid use of deprecated TreeBuilder instantiation

--- AdapterFactory.php
+++ AdapterFactory.php
@@ -8,7 +8,7 @@ class AdapterFactory
 {
     public static function create(string $type, array $config): StorageAdapterInterface
     {
-        $class = '\\ObjectStorage\\Adapter\\' . ucfirst($type) . 'Adapter';
+        $class = '\\ObjectStorage\\Adapter\\' . \ucfirst($type) . 'Adapter';
         if (!\class_exists($class)) {
             throw new \RuntimeException(
                 "Unable to create a \"{$type}\" Storage Adapter because the class ({$class}) does not exist or could not be loaded."
--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -9,8 +9,8 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('object_storage');
+        $treeBuilder = new TreeBuilder('object_storage');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
